47d18ff4dc9b27dd31fea618f2caa745229e64aa
Bharath Singh <bharathsingh11.bs@gmail.com>
2025-03-17 19:32:34 +0530

    add image url for attention markets

--- backend/markets/models.py
+++ backend/markets/models.py
@@ -7,4 +7,5 @@ from tools.app_models import TimeTrackedModel
 
 class AttentionMarket(TimeTrackedModel):
     slug = models.CharField(max_length=255, unique=True)
+    image_url = models.CharField(max_length=255, null=True)
     address = models.CharField(max_length=255)
