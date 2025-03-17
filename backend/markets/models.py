dc3ca05127c7ac6863b83a58a43348e27597188f
Bharath Singh <bharathsingh11.bs@gmail.com>
2025-03-17 18:50:34 +0530

    add model for attention markets

--- backend/markets/models.py
+++ backend/markets/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
+from tools.app_models import TimeTrackedModel
+
 # Create your models here.
+
+
+class AttentionMarket(TimeTrackedModel):
+    slug = models.CharField(max_length=255, unique=True)
+    address = models.CharField(max_length=255)
