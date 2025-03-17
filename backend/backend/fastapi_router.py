68a31ae32c83d7e6f8543533d18165b0166bc72b
Bharath Singh <bharathsingh11.bs@gmail.com>
2025-03-17 18:42:25 +0530

    add markets app

--- backend/backend/fastapi_router.py
+++ backend/backend/fastapi_router.py
@@ -1,5 +1,7 @@
 from fastapi import FastAPI
+from markets.views import router as market_router
 
 
 def setup_routers(app: FastAPI):
     """Routes"""
+    app.include_router(market_router, prefix="/markets")
